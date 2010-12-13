e4612bc8d1dae760a7a507b886b99ac3c74caf05
Nathen Harvey <nharvey@customink.com>
2010-12-13 10:53:18 -0500

    Update the initializer so that it will check for HoptoadNotifier before trying to configure it.
    This is helpful when the notifier isn't installed in an environment, such as development

--- generators/hoptoad/templates/initializer.rb
+++ generators/hoptoad/templates/initializer.rb
@@ -1,6 +1,8 @@
 <% if Rails::VERSION::MAJOR < 3 && Rails::VERSION::MINOR < 2 -%>
 require 'hoptoad_notifier/rails'
 <% end -%>
-HoptoadNotifier.configure do |config|
-  config.api_key = <%= api_key_expression %>
+if defined?(HoptoadNotifier)
+  HoptoadNotifier.configure do |config|
+    config.api_key = <%= api_key_expression %>
+  end
 end
